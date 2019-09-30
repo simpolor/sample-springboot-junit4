4b8c3917cd725a0cbb3f5b5bea30152d5765ab88
parksy <parksy@smilegate.com>
2019-09-30 18:00:06 +0900

    .

--- src/test/java/io/simpolor/testing/controller/DemoWebMvcTest.java
+++ src/test/java/io/simpolor/testing/controller/DemoWebMvcTest.java
@@ -1,17 +1,22 @@
 package io.simpolor.testing.controller;
 
 import io.simpolor.testing.service.DemoService;
-import org.junit.jupiter.api.Test;
+// import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 // @WebMvcTest
+@RunWith(SpringRunner.class)
 @WebMvcTest(controllers = {DemoController.class}) // 특정 컨트롤러만 로드
 public class DemoWebMvcTest {
 
@@ -29,6 +34,19 @@ public class DemoWebMvcTest {
         this.mockMvc.perform(get("/demo/totalcount"))
                 .andDo(MockMvcResultHandlers.print())
                 .andExpect(status().isOk());
+                // .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                // .andExpect(jsonPath("$.person.name").value("Jason"))
+                // .andExpect(redirectedUrl("/messages/123"));
+
+
+        /* this.mockMvc.perform(post("/demo/totalcount"))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(status().isOk())
+                .andExpect(redirectedUrl("/person/1"))
+                .andExpect(model().size(1))
+                .andExpect(model().attributeExists("person"))
+                .andExpect(flash().attributeCount(1))
+                .andExpect(flash().attribute("message", "success!")); */
 
     }
 
