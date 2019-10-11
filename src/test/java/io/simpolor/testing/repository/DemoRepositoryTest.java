1dc8db77da6e8004218ff9975d1fc3f9674c351c
parksy <parksy@smilegate.com>
2019-10-11 16:25:13 +0900

    .

---
+++ src/test/java/io/simpolor/testing/repository/DemoRepositoryTest.java
@@ -0,0 +1,28 @@
+package io.simpolor.testing.repository;
+
+import org.junit.*;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {DemoRepository.class})
+public class DemoRepositoryTest {
+
+	@Autowired
+	private DemoRepository demoRepository;
+
+	@Test
+	public void testSelectDemoTotalcount() {
+
+		// when
+		long result = demoRepository.count();
+
+
+		// then
+		Assert.assertNotNull(result);
+		Assert.assertEquals(3L, result);
+	}
+
+}
