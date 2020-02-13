7b2d04644c6bf4a3e47721cd65168c207db36abc
parksy <parksy@smilegate.com>
2020-02-13 16:49:09 +0900

    void에 대한 테스트 코드 추가

---
+++ src/test/java/io/simpolor/testing/service/DemoServiceVoidTest.java
@@ -0,0 +1,56 @@
+package io.simpolor.testing.service;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.*;
+
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+public class DemoServiceVoidTest {
+
+    @Mock
+    private DemoService demoService;
+
+    @Before
+    public void initMocks() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void testDemoVisit() {
+
+        // given
+        long visit = 3L;
+
+
+        // when
+        demoService.setVisit(visit);
+
+
+        // then
+        verify(demoService, times(1)).setVisit(anyLong()); // success
+
+    }
+
+    @Test
+    public void testDemoCaptor() {
+
+        // given
+        long visit = 3L;
+
+
+        // when
+        demoService.setVisit(visit);
+
+
+        // then
+        ArgumentCaptor<Long> visitCapture = ArgumentCaptor.forClass(Long.class);
+        verify(demoService, times(1)).setVisit(visitCapture.capture()); // success
+
+        Assertions.assertThat(visitCapture.getValue()).isEqualTo(visit);
+    }
+
+}
