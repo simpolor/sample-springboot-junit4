51ae14648ed6ca833736b93431dbd910f2a1f031
parksy <parksy@smilegate.com>
2020-04-21 17:28:33 +0900

    [add] StopWatch 관련 테스트 코드 추가

---
+++ src/test/java/io/simpolor/testing/controller/DemoStopWatchTest.java
@@ -0,0 +1,61 @@
+package io.simpolor.testing.controller;
+
+import io.simpolor.testing.TestingApplication;
+import io.simpolor.testing.domain.Demo;
+import org.junit.*;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.StopWatch;
+
+import java.math.BigDecimal;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = TestingApplication.class , webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class DemoStopWatchTest {
+
+    private StopWatch stopWatch;
+
+    @Autowired
+    private DemoController demoController;
+
+    @Before
+    public void setUp() {
+        stopWatch = new StopWatch("simpolor");
+    }
+
+
+    @Test
+    public void testDemoIntergration(){
+
+        // given
+        BigDecimal bigDecimal = BigDecimal.valueOf(0, 0);
+        Long longType = 0L;
+
+        long seq = 1;
+
+
+        // when
+        Demo result = demoController.demoView(seq);
+
+        stopWatch.start("Long type");
+        for(int i = 0; i < 1_000_000; i++) {
+            longType += 1L;
+        }
+        stopWatch.stop();
+
+        stopWatch.start("BigDecimal type");
+        for(int i = 0; i < 1_000_000; i++) {
+            bigDecimal = bigDecimal.add(BigDecimal.ONE);
+        }
+        stopWatch.stop();
+
+        // then
+        Assert.assertNotNull(result);
+        System.out.println("stopWatch.shortSummary : "+stopWatch.shortSummary());
+        System.out.println("stopWatch.getTotalTimeMillis : "+stopWatch.getTotalTimeMillis());
+        System.out.println("stopWatch.prettyPrint : "+stopWatch.prettyPrint());
+    }
+
+}
