c158afa30233a9a90903167b17dc4ec4cf0e9b4a
parksy <parksy@smilegate.com>
2020-07-21 18:01:06 +0900

    [MOD] @SpringBootTest에 @RunWith 어노테이션 추가

--- src/test/java/io/simpolor/testing/service/DemoServiceMockBeanTest.java
+++ src/test/java/io/simpolor/testing/service/DemoServiceMockBeanTest.java
@@ -3,12 +3,15 @@ package io.simpolor.testing.service;
 import io.simpolor.testing.repository.DemoRepository;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.mockito.Mockito.when;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest(classes = {DemoService.class})
 public class DemoServiceMockBeanTest {
 
